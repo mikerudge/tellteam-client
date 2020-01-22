c05a8c2200d30574858f5352cb39bee9f917b93c
Mike Rudge <rudge@wearepropeller.com>
2020-01-22 22:01:05 +0000

    user search
    Signed-off-by: Mike Rudge <rudge@wearepropeller.com>

--- src/components/UserSearchInput/UserSearchInput.tsx
+++ src/components/UserSearchInput/UserSearchInput.tsx
@@ -1,47 +1,70 @@
 import React, { useState } from 'react';
 import { Select, Spin, message } from 'antd';
-import { useQuery } from 'react-apollo';
+import { useParams } from 'react-router-dom';
+import debounce from 'lodash/debounce';
+import { useQuery, useLazyQuery } from 'react-apollo';
 import { GET_ACCOUNT_MEMBERS } from '../../shared/graphql';
-import { Get_Account_MembersQuery, Get_Account_MembersQueryVariables } from '../../shared/API_TYPES';
+import { Get_Account_MembersQuery, Get_Account_MembersQueryVariables, User } from '../../shared/API_TYPES';
+import { SEARCH_USER } from './query';
 const { Option } = Select;
-
-type User = {
-  value: string;
-  text: string;
-};
 
 const UserSearchInput: React.FC = () => {
   const [member, setMember] = useState();
+  const { id } = useParams();
 
-  const { data, loading, error } = useQuery<Get_Account_MembersQuery, Get_Account_MembersQueryVariables>(
-    GET_ACCOUNT_MEMBERS,
-    { variables: { id: 'ck56yfrae01cg07jma361drto' } }
-  );
+  // const { data, loading, error } = useQuery<Get_Account_MembersQuery, Get_Account_MembersQueryVariables>(
+  //   GET_ACCOUNT_MEMBERS,
+  //   { variables: { id: id || '' } }
+  // );
+
+  const [searchUser, { loading, data, error }] = useLazyQuery(SEARCH_USER);
 
   if (error) {
     message.error('Something went wrong!');
     return null;
   }
 
-  const members = data?.account?.members?.items ?? [];
+  const members: User[] = data?.usersList?.items ?? [];
+
+  const fetchUser = (v: string) => {
+    searchUser({ variables: { query: v } });
+  };
+
+  const handleChange = (value: string) => {
+    console.log('called');
+    console.log(value);
+    setMember(value);
+  };
+
+  const results = members.map(member => {
+    return {
+      text: `${member?.firstName} ${member.lastName}`,
+      value: member.id,
+    };
+  });
 
   return (
     <Select
       size="large"
+      mode="multiple"
       labelInValue
       value={member}
       placeholder="Select members"
       notFoundContent={loading ? <Spin size="small" /> : null}
       filterOption={false}
-      onChange={(e: React.SetStateAction<string>) => setMember(e)}
+      onSearch={debounce(fetchUser, 800)}
+      onChange={handleChange}
       style={{ width: '100%' }}
     >
       {members &&
-        members.map(d => (
-          <Option key={d.id ?? 'djs'}>
-            {d.firstName} {d.lastName}
-          </Option>
-        ))}
+        members.map((d: User) => {
+          console.log('d', d);
+          return (
+            <Option key={d.id ?? 'noId'}>
+              {d.firstName} {d.lastName}
+            </Option>
+          );
+        })}
     </Select>
   );
 };
